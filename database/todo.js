568246e36689f087c4fe8e14f2ae5d28989d3594
Ron <103109647+hwk246@users.noreply.github.com>
2022-06-08 17:24:00 +0200

    Add files via upload

---
+++ database/todo.js
@@ -0,0 +1,1 @@
+export const _ = [ { 'description': `New task almost done`, 'done': false, '_id': `9e89aec4-100c-4388-83d9-8eb5559ab669`, '_createdOn': `2022-06-08T15:04:59.786Z`, '_updatedOn': `2022-06-08T15:05:45.371Z` } ];
